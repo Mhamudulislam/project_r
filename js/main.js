86b94365f0bc0c8f43f5522e4c3c5aac45172b62
Mhamudulislam <141499999+Mhamudulislam@users.noreply.github.com>
2023-11-17 20:33:41 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+var any1=20;
+var any2=50;
+var result = any1 + any2;
+function anythink()
+{document.getElementById('plase').innerHTML=any1;}
+
+
+
+let num1s, num2s;
+num1s=50;
+num2s=40;
+num1s += num2s;
+function anythin()
+{document.getElementById('plasee').innerHTML=num1s;}
